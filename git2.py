b345fb45affe1be6cb88337e668da00834feb72c
szocsg <szocsg@irinyi-ref.hu>
2025-03-18 14:05:10 +0100

    Három soros bővítés.

--- git2.py
+++ git2.py
@@ -2,5 +2,8 @@ print("Hello py git!")
 a=5
 b=2
 print(a+b)
+a=int(input("Adj meg egy egész számot: "))
+b=int(input("Adj meg még egy egész számot: "))
+print(f"Összegük: {a+b}")
 for i in range(3, 0, -1):
     print(f"Még {i} hét!")
